94171620bd66baeebcc4f6a48a2d2b02b6dd5f95
[Maxime CodeWeb] <[coquelin.maxime@yahoo.com]>
2022-11-10 21:13:12 +0100

    AvancementSecondCard

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
+    colors: {
+      'myColor': '#312014',
+      'wh': 'FFFFFF',
+    },
     extend: {
       fontFamily: {
         'pop': ['"Poppins"', 'modern']
