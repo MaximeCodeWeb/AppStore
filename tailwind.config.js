dca954e982d8244801c054292c49a0193a392cbf
[Maxime CodeWeb] <[coquelin.maxime@yahoo.com]>
2022-11-11 15:36:23 +0100

    ButtonBuy

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,17 @@ module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
     colors: {
-      'myColor': '#312014',
-      'wh': 'FFFFFF',
+      'myColor': '#BED3C3',
+      'white': 'FFFFFF',
+      'blue': '#1fb6ff',
+      'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'orange': '#FF8C2E',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6'
     },
     extend: {
       fontFamily: {
